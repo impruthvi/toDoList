d3f1582bde3799bf5590934fad7115fcbfac8988
Pruthvi <pruthvirajput97@gmail.com>
2020-11-15 18:28:23 +0530

    add work list page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = [];
+let workItems=[];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,13 +20,33 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("eg-US", options);
 
-  res.render("list", { kidOfday: day, newListItem: items });
+  res.render("list", { listTitle: day, newListItem: items });
 });
 
 app.post("/", function (req, res) {
+
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    
+    items.push(item);
+    res.redirect("/");
+  }
+  
+
+});
+
+
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work List",newListItem:workItems})
+})
+
+app.post("/work",function(req,res){
+  let item = req.body.newItem;
+  item.push(workItems);
+  res.redirect("/work");
 });
 
 app.listen(3000, function () {
