17eaad2c5ffac67c09c294eb8ec875b998520ed9
Pruthvi <pruthvirajput97@gmail.com>
2020-12-04 22:11:49 +0530

    Adding New Items to the Custom ToDo Lists

--- app.js
+++ app.js
@@ -60,13 +60,25 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: itemName,
   });
 
-  item.save();
-  res.redirect("/");
+
+  if(listName==='Today'){
+    item.save();
+    res.redirect("/");
+  }else{
+    List.findOne({name:listName},function(err,foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect('/'+listName);
+    })
+  }
+
+ 
 });
 
 app.post("/delete", function (req, res) {
